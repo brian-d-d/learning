f0e59ef7b2880f305f9f0561cdc0b3e3dd6df0c9
briand <brian.dortpinar@zenobe.com>
2024-02-01 14:00:03 +1100

    step9 done

---
+++ cmake-3.28.2-tutorial-source/Step9/Step9_Build/_CPack_Packages/-Source/TGZ/--Source/Step9_Build/CTestTestfile.cmake
@@ -0,0 +1,25 @@
+# CMake generated Testfile for 
+# Source directory: /Users/admin/Documents/learning/cmake-3.28.2-tutorial-source/Step9
+# Build directory: /Users/admin/Documents/learning/cmake-3.28.2-tutorial-source/Step9/Step9_Build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(Runs "/Users/admin/Documents/learning/cmake-3.28.2-tutorial-source/Step9/Step9_Build/Tutorial" "25")
+set_tests_properties(Runs PROPERTIES  _BACKTRACE_TRIPLES "/Users/admin/Documents/learning/cmake-3.28.2-tutorial-source/Step9/CMakeLists.txt;54;add_test;/Users/admin/Documents/learning/cmake-3.28.2-tutorial-source/Step9/CMakeLists.txt;0;")
+add_test(Usage "/Users/admin/Documents/learning/cmake-3.28.2-tutorial-source/Step9/Step9_Build/Tutorial")
+set_tests_properties(Usage PROPERTIES  PASS_REGULAR_EXPRESSION "Usage:.*number" _BACKTRACE_TRIPLES "/Users/admin/Documents/learning/cmake-3.28.2-tutorial-source/Step9/CMakeLists.txt;57;add_test;/Users/admin/Documents/learning/cmake-3.28.2-tutorial-source/Step9/CMakeLists.txt;0;")
+add_test(Comp4 "/Users/admin/Documents/learning/cmake-3.28.2-tutorial-source/Step9/Step9_Build/Tutorial" "4")
+set_tests_properties(Comp4 PROPERTIES  PASS_REGULAR_EXPRESSION "4 is 2" _BACKTRACE_TRIPLES "/Users/admin/Documents/learning/cmake-3.28.2-tutorial-source/Step9/CMakeLists.txt;64;add_test;/Users/admin/Documents/learning/cmake-3.28.2-tutorial-source/Step9/CMakeLists.txt;71;do_test;/Users/admin/Documents/learning/cmake-3.28.2-tutorial-source/Step9/CMakeLists.txt;0;")
+add_test(Comp9 "/Users/admin/Documents/learning/cmake-3.28.2-tutorial-source/Step9/Step9_Build/Tutorial" "9")
+set_tests_properties(Comp9 PROPERTIES  PASS_REGULAR_EXPRESSION "9 is 3" _BACKTRACE_TRIPLES "/Users/admin/Documents/learning/cmake-3.28.2-tutorial-source/Step9/CMakeLists.txt;64;add_test;/Users/admin/Documents/learning/cmake-3.28.2-tutorial-source/Step9/CMakeLists.txt;72;do_test;/Users/admin/Documents/learning/cmake-3.28.2-tutorial-source/Step9/CMakeLists.txt;0;")
+add_test(Comp5 "/Users/admin/Documents/learning/cmake-3.28.2-tutorial-source/Step9/Step9_Build/Tutorial" "5")
+set_tests_properties(Comp5 PROPERTIES  PASS_REGULAR_EXPRESSION "5 is 2.236" _BACKTRACE_TRIPLES "/Users/admin/Documents/learning/cmake-3.28.2-tutorial-source/Step9/CMakeLists.txt;64;add_test;/Users/admin/Documents/learning/cmake-3.28.2-tutorial-source/Step9/CMakeLists.txt;73;do_test;/Users/admin/Documents/learning/cmake-3.28.2-tutorial-source/Step9/CMakeLists.txt;0;")
+add_test(Comp7 "/Users/admin/Documents/learning/cmake-3.28.2-tutorial-source/Step9/Step9_Build/Tutorial" "7")
+set_tests_properties(Comp7 PROPERTIES  PASS_REGULAR_EXPRESSION "7 is 2.645" _BACKTRACE_TRIPLES "/Users/admin/Documents/learning/cmake-3.28.2-tutorial-source/Step9/CMakeLists.txt;64;add_test;/Users/admin/Documents/learning/cmake-3.28.2-tutorial-source/Step9/CMakeLists.txt;74;do_test;/Users/admin/Documents/learning/cmake-3.28.2-tutorial-source/Step9/CMakeLists.txt;0;")
+add_test(Comp25 "/Users/admin/Documents/learning/cmake-3.28.2-tutorial-source/Step9/Step9_Build/Tutorial" "25")
+set_tests_properties(Comp25 PROPERTIES  PASS_REGULAR_EXPRESSION "25 is 5" _BACKTRACE_TRIPLES "/Users/admin/Documents/learning/cmake-3.28.2-tutorial-source/Step9/CMakeLists.txt;64;add_test;/Users/admin/Documents/learning/cmake-3.28.2-tutorial-source/Step9/CMakeLists.txt;75;do_test;/Users/admin/Documents/learning/cmake-3.28.2-tutorial-source/Step9/CMakeLists.txt;0;")
+add_test(Comp-25 "/Users/admin/Documents/learning/cmake-3.28.2-tutorial-source/Step9/Step9_Build/Tutorial" "-25")
+set_tests_properties(Comp-25 PROPERTIES  PASS_REGULAR_EXPRESSION "-25 is (-nan|nan|0)" _BACKTRACE_TRIPLES "/Users/admin/Documents/learning/cmake-3.28.2-tutorial-source/Step9/CMakeLists.txt;64;add_test;/Users/admin/Documents/learning/cmake-3.28.2-tutorial-source/Step9/CMakeLists.txt;76;do_test;/Users/admin/Documents/learning/cmake-3.28.2-tutorial-source/Step9/CMakeLists.txt;0;")
+add_test(Comp0.0001 "/Users/admin/Documents/learning/cmake-3.28.2-tutorial-source/Step9/Step9_Build/Tutorial" "0.0001")
+set_tests_properties(Comp0.0001 PROPERTIES  PASS_REGULAR_EXPRESSION "0.0001 is 0.01" _BACKTRACE_TRIPLES "/Users/admin/Documents/learning/cmake-3.28.2-tutorial-source/Step9/CMakeLists.txt;64;add_test;/Users/admin/Documents/learning/cmake-3.28.2-tutorial-source/Step9/CMakeLists.txt;77;do_test;/Users/admin/Documents/learning/cmake-3.28.2-tutorial-source/Step9/CMakeLists.txt;0;")
+subdirs("MathFunctions")
